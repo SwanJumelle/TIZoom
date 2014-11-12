6a5b9ebb320f6e3f8380d8b0ced6a3a3e530bedd
Thomas Montana <thmsmontana@gmail.com>
2014-11-12 14:23:50 +0100

    Reset the project

---
+++ app/src/main/java/com/coloc/tizoom/PhotoDisplayActivity.java
@@ -0,0 +1,53 @@
+package com.coloc.tizoom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class PhotoDisplayActivity extends Activity {
+
+    protected static final int REQUEST_OK = 1;
+    private long lastUpdate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_display);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.photo_display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+        try {
+            startActivityForResult(i, REQUEST_OK);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error initializing speech to text engine.", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/coloc/tizoom/ZoomablePhotoView.java
@@ -0,0 +1,11 @@
+package com.coloc.tizoom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ZoomablePhotoView extends View {
+    public ZoomablePhotoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
